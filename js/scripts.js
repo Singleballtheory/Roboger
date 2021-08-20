f33e07aabe39d96d83648d813ff4be58cdf20531
Scott Hutley <scotthutley1@comcast.net>
2021-08-20 14:40:42 -0700

    replace 1st if statement with line 10 and 11 code; WORKS!

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 function beepBoop(input) {
   let output = []
   for (i = 0; i <= input; i++){
-    if (i === 3) {
+    if (i.toString().includes(3)) {
       output.push("Won't you be my neighbor?");
     } else if (i === 2) {
       output.push("Boop!");
     } else if (i === 1) {
       output.push("Beep!");
-    } else if (i.toString().includes(3)) {
-      output.push("Won't you be my neighbor?");
+    // } else if (i.toString().includes(3)) {
+    //   output.push("Won't you be my neighbor?");
     } else {
     output.push(i);
     }
